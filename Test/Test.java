21cd52ee293b7968847b7fd1ac3f0e40bfc7f67d
VerdiX <VerdiX@192.168.43.227>
2017-03-02 09:13:58 +0600

    добавил тест разности

---
+++ Test/Test.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import Calculjator.a.Calc_oper;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		double res = Calc_oper.C_op(10, "-", 5);//присваевается результат от метода C_op класса Calc_oper
+		assertEquals(5,res,0);//сравнивается ожидаемый результат с результатом от метода C_op класса Calc_oper
+	}
+
+}
